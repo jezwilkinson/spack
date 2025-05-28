b41d80db8b3995ce64949f8f34d9a7defbe9d5fe
Jeremy Wilkinson <jeremy.wilkinson@cern.ch>
2025-05-28 14:43:21 +0200

    revert openBLAS makefile change

--- var/spack/repos/spack_repo/builtin/packages/openblas/package.py
+++ var/spack/repos/spack_repo/builtin/packages/openblas/package.py
@@ -549,7 +549,7 @@ class MakefileBuilder(makefile.MakefileBuilder):
 
     @property
     def build_targets(self):
-        return ["-s"] + self.make_defs + ["shared"]
+        return ["-s"] + self.make_defs + ["all"]
 
     @run_after("build")
     @on_package_attributes(run_tests=True)
